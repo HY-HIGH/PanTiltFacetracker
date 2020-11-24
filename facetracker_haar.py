186c6d888524cf9ea90c09a27b89d9ed788ffdfd
kissw <kiminstein@naver.com>
2020-11-24 13:23:46 +0900

    .

--- facetracker_haar.py
+++ facetracker_haar.py
@@ -4,14 +4,14 @@
 Modified from code posted here: http://forums.pimoroni.com/t/pan-tilt-hat-repo/3402/11
 """
 import numpy as np
-import cv2 as cv
+import cv2
 import os
 from pantilthat import *
 
 os.system('sudo modprobe bcm2835-v4l2')
 
-cascade = cv.Load('./data/haarcascades/haarcascade_frontalface_default.xml')
-#cascade = cv.Load('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
+cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_frontalface_default.xml')
+#cascade = cv2.Load('/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml')
 
 cam_pan = 90
 cam_tilt = 45
@@ -32,56 +32,56 @@ min_size = (15, 15)
 image_scale = 5
 haar_scale = 1.2
 min_neighbors = 2
-haar_flags = cv.CV_HAAR_DO_CANNY_PRUNING
+haar_flags = cv2.CV_HAAR_DO_CANNY_PRUNING
 
-cap = cv.CreateCameraCapture(0)
-cv.NamedWindow("Tracker", 1)
+cap = cv2.CreateCameraCapture(0)
+cv2.NamedWindow("Tracker", 1)
  
 if cap:
     frame_copy = None
     
 while(True):
     # Capture frame-by-frame
-    frame = cv.QueryFrame(cap)
+    frame = cv2.QueryFrame(cap)
     if not frame:
-        cv.WaitKey(0)
+        cv2.WaitKey(0)
         break
     if not frame_copy:
-        frame_copy = cv.CreateImage((frame.width,frame.height),
-                                            cv.IPL_DEPTH_8U, frame.nChannels)
-    if frame.origin == cv.IPL_ORIGIN_TL:
-        cv.Flip(frame, frame, -1)
+        frame_copy = cv2.CreateImage((frame.width,frame.height),
+                                            cv2.IPL_DEPTH_8U, frame.nChannels)
+    if frame.origin == cv2.IPL_ORIGIN_TL:
+        cv2.Flip(frame, frame, -1)
    
     # Our operations on the frame come here
-    gray = cv.CreateImage((frame.width,frame.height), 8, 1)
-    small_img = cv.CreateImage((cv.Round(frame.width / image_scale),
-                   cv.Round (frame.height / image_scale)), 8, 1)
+    gray = cv2.CreateImage((frame.width,frame.height), 8, 1)
+    small_img = cv2.CreateImage((cv2.Round(frame.width / image_scale),
+                   cv2.Round (frame.height / image_scale)), 8, 1)
  
     # convert color input image to grayscale
-    cv.CvtColor(frame, gray, cv.CV_BGR2GRAY)
+    cv2.CvtColor(frame, gray, cv2.CV_BGR2GRAY)
  
     # scale input image for faster processing
-    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+    cv2.Resize(gray, small_img, cv2.CV_INTER_LINEAR)
  
-    cv.EqualizeHist(small_img, small_img)
+    cv2.EqualizeHist(small_img, small_img)
 
     midFace = None
  
     if(cascade):
-        t = cv.GetTickCount()
+        t = cv2.GetTickCount()
         # HaarDetectObjects takes 0.02s
-        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
+        faces = cv2.HaarDetectObjects(small_img, cascade, cv2.CreateMemStorage(0),
                                      haar_scale, min_neighbors, haar_flags, min_size)
-        t = cv.GetTickCount() - t
+        t = cv2.GetTickCount() - t
         if faces:
             lights(50 if len(faces) == 0 else 0, 50 if len(faces) > 0 else 0,0,50)
 
             for ((x, y, w, h), n) in faces:
-                # the input to cv.HaarDetectObjects was resized, so scale the
+                # the input to cv2.HaarDetectObjects was resized, so scale the
                 # bounding box of each face and convert it to two CvPoints
                 pt1 = (int(x * image_scale), int(y * image_scale))
                 pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
-                cv.Rectangle(frame, pt1, pt2, cv.RGB(100, 220, 255), 1, 8, 0)
+                cv2.Rectangle(frame, pt1, pt2, cv2.RGB(100, 220, 255), 1, 8, 0)
                 # get the xy corner co-ords, calc the midFace location
                 x1 = pt1[0]
                 x2 = pt2[0]
@@ -109,9 +109,9 @@ while(True):
                 break
                 
     # Display the resulting frame
-    cv.ShowImage('Tracker',frame)
-    if cv.WaitKey(1) & 0xFF == ord('q'):
+    cv2.ShowImage('Tracker',frame)
+    if cv2.WaitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-cv.DestroyWindow("Tracker")
+cv2.DestroyWindow("Tracker")
