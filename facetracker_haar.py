e0325ba8660b583c418a0b81c7671d3b49cfaa62
kissw <kiminstein@naver.com>
2020-11-24 13:24:28 +0900

    .

--- facetracker_haar.py
+++ facetracker_haar.py
@@ -32,7 +32,7 @@ min_size = (15, 15)
 image_scale = 5
 haar_scale = 1.2
 min_neighbors = 2
-haar_flags = cv2.CV_HAAR_DO_CANNY_PRUNING
+haar_flags = cv2.cv.CV_HAAR_DO_CANNY_PRUNING
 
 cap = cv2.CreateCameraCapture(0)
 cv2.NamedWindow("Tracker", 1)
